d584762bd9f354a624f61040bb79d2e5602cbb04
Eric Lippmann <eric.lippmann@icinga.com>
2026-02-10 11:54:34 +0100

    Remove Zend/Service*

--- tests/Zend/AllTests.php
+++ tests/Zend/AllTests.php
@@ -89,7 +89,6 @@ require_once 'Zend/Queue/AllTests.php';
 require_once 'Zend/Search/Lucene/AllTests.php';
 require_once 'Zend/Serializer/AllTests.php';
 require_once 'Zend/Server/AllTests.php';
-require_once 'Zend/Service/AllTests.php';
 // require_once 'Zend/Session/AllTests.php';
 // require_once 'Zend/Soap/AllTests.php';
 require_once 'Zend/Tag/AllTests.php';
@@ -237,7 +236,6 @@ class Zend_AllTests
         // $suite->addTest(Zend_Search_Lucene_AllTests::suite());
         $suite->addTest(Zend_Serializer_AllTests::suite());
         // $suite->addTest(Zend_Server_AllTests::suite());
-        // $suite->addTest(Zend_Service_AllTests::suite());
         $suite->addTest(Zend_Tag_AllTests::suite());
         // $suite->addTest(Zend_Test_AllTests::suite());
         $suite->addTest(Zend_Text_AllTests::suite());
