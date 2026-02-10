157f9a30d4693a800eb8f2dd458cd9bcafb77c78
Eric Lippmann <eric.lippmann@icinga.com>
2026-02-10 12:01:32 +0100

    Remove Zend/Rest*

--- tests/Zend/AllTests.php
+++ tests/Zend/AllTests.php
@@ -77,7 +77,6 @@ require_once 'Zend/Navigation/AllTests.php';
 require_once 'Zend/Paginator/AllTests.php';
 require_once 'Zend/ProgressBar/AllTests.php';
 require_once 'Zend/RegistryTest.php';
-require_once 'Zend/Rest/AllTests.php';
 // require_once 'Zend/Session/AllTests.php';
 require_once 'Zend/Tag/AllTests.php';
 // require_once 'Zend/Test/AllTests.php';
@@ -209,7 +208,6 @@ class Zend_AllTests
         $suite->addTest(Zend_Paginator_AllTests::suite());
         $suite->addTest(Zend_ProgressBar_AllTests::suite());
         $suite->addTestSuite('Zend_RegistryTest');
-        $suite->addTest(Zend_Rest_AllTests::suite());
         $suite->addTest(Zend_Tag_AllTests::suite());
         // $suite->addTest(Zend_Test_AllTests::suite());
         $suite->addTest(Zend_Text_AllTests::suite());
