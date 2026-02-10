fabb0a4bc130f689af724691d8eba21f86ac2153
Eric Lippmann <eric.lippmann@icinga.com>
2026-02-10 11:56:16 +0100

    Remove Zend/Search*

--- tests/Zend/AllTests.php
+++ tests/Zend/AllTests.php
@@ -86,7 +86,6 @@ require_once 'Zend/Reflection/AllTests.php';
 require_once 'Zend/RegistryTest.php';
 require_once 'Zend/Rest/AllTests.php';
 require_once 'Zend/Queue/AllTests.php';
-require_once 'Zend/Search/Lucene/AllTests.php';
 require_once 'Zend/Serializer/AllTests.php';
 require_once 'Zend/Server/AllTests.php';
 // require_once 'Zend/Session/AllTests.php';
@@ -230,7 +229,6 @@ class Zend_AllTests
         $suite->addTest(Zend_Rest_AllTests::suite());
         $suite->addTest(Zend_Reflection_AllTests::suite());
         $suite->addTest(Zend_Queue_AllTests::suite());
-        // $suite->addTest(Zend_Search_Lucene_AllTests::suite());
         $suite->addTest(Zend_Serializer_AllTests::suite());
         // $suite->addTest(Zend_Server_AllTests::suite());
         $suite->addTest(Zend_Tag_AllTests::suite());
