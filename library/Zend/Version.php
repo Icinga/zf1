7acc93188de48501cccadb7f223c0aebeec5bf57
Johannes Meyer <johannes.meyer@icinga.com>
2026-03-31 16:07:58 +0200

    Release version 2.0.1

--- library/Zend/Version.php
+++ library/Zend/Version.php
@@ -32,7 +32,7 @@ final class Zend_Version
     /**
      * Zend Framework version identification - see compareVersion()
      */
-    public const VERSION = '2.0.0';
+    public const VERSION = '2.0.1';
 
     /**
      * The latest stable version Zend Framework available
